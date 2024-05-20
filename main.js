d2d7da7cc332eef5da38f6a6adb3ce114d3b774d
Brenda Lima <156533195+brndlima@users.noreply.github.com>
2024-05-20 06:38:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+
+    else {
+                //alert('Elemento não encontrado');
+        console.log('Elemento nao encontrado ou seletor invalido')
+            elemento.play();
+        
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+}
